db1958eb73b549c240e148d3e77a4a8174c30f51
Ignacio Moran <moran.ua@gmail.com>
2021-06-06 17:45:59 -0400

    before merge

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    # @user.password_digest = BCrypt::Password.create(params[:user][:password])
+    #@user.password_digest = BCrypt::Password.create(params[:user][:password])
     if @user.save
       session[:user_id] = @user.id
       redirect_to root_path, notice: 'User was successfully created.'
